0efca3d4d8fa47c52e0047fa4ccccac7782a8282
siddhi-kabadi <121832255+siddhi-kabadi@users.noreply.github.com>
2024-08-03 16:43:08 -0700

    files upload

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-// Add smooth scrolling to all anchor links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
+function toggleMenu()
+{
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  const bars = document.querySelectorAll('.progress-bar');
+  bars.forEach(bar => {
+      let width = bar.style.width;
+      bar.style.width = '0%';
+      setTimeout(() => {
+          bar.style.width = width;
+      }, 100);
+  });
 });
+
